80335303127bf695fe1471a860826220b310291f
Jan-Erik Rediger <badboy@archlinux.us>
2014-04-03 21:29:16 +0200

    Removed old test code

--- main.go
+++ main.go
@@ -6,24 +6,6 @@ import (
 )
 
 func main() {
-	//n := NewNode("localhost:7001")
-	//fmt.Println("hello", n.slots)
-	//fmt.Println("dead?", n.IsDead())
-	//fmt.Println("alive?", n.IsAlive())
-	//fmt.Println("cluster enabled?", n.IsClusterEnabled())
-	//fmt.Println("empty?", n.IsEmpty())
-
-	//c := NewCluster("localhost:7001")
-	//fmt.Println("hello", c)
-	//fmt.Println("nodes", c.FetchNodes())
-
-	//c.Each(func(n *Node, result interface{}, err error, cmd string, args []interface{}) {
-	//val, _ := redis.String(result, err)
-	//fmt.Printf("%s: %s %s\n", n.String(), cmd, args)
-	//fmt.Print(val)
-	//fmt.Println("--")
-	//}, "INFO", "memory")
-
 	if len(os.Args) < 3 {
 		fmt.Printf("usage: %s [command] [ip:port] [arguments]\n", os.Args[0])
 		os.Exit(1)
